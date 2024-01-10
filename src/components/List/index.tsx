b3ff81017dbf976aae44f463187bbfc1fc27d94a
Vlad Solovey <connectwithvlad@gmail.com>
2024-01-10 20:59:16 +0300

    refactor: remove useless tasks length check

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -6,11 +6,9 @@ import classes from './index.module.css';
 const List = () => {
   const { tasks } = useTasksContext();
 
-  const hasTasks = Boolean(tasks.length);
-
   return (
     <div className={classes.list}>
-      {hasTasks && tasks.map(task => (
+      {tasks.map(task => (
         <TaskContainer
           key={task.id}
           task={task}
